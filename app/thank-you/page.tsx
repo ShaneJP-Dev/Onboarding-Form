76c7232a254e3660a7c237e7e017155acc4dce05
Shane Pelser <shanejpelser.dev@gmail.com>
2024-12-16 11:11:16 +0200

    Inittial
    Complete form, with email and file upload

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ThankYouPage = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Thank You!</h1>
+      <p>We have received your submission. We will contact you shortly.</p>
+    </div>
+  );
+};
+
+export default ThankYouPage;
